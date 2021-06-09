425bf80e4565eb686b4f8fba8340197ca00dd0b6
Bjarkebek <bjarke44@gmail.com>
2021-06-09 13:50:57 +0200

    13:50

--- LAX_AS_ASP/Controllers/HomeController.cs
+++ LAX_AS_ASP/Controllers/HomeController.cs
@@ -1,57 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using LAX_AS_ASP.Models;
 
 namespace LAX_AS_ASP.Controllers
 {
     public class HomeController : Controller
     {
-        public class Film
-        {
-            public string FilmTitel { get; set; }
-            public string FilmInstruk { get; set; }
-            public string FilmUdgiv { get; set; }
-            public string FilmOm { get; set; }
-            public string FilmNomi { get; set; }
-            public string NomiVundet { get; set; }
-        }
-
+        
         public ActionResult Index()
         {
+            List<FilmDB> Film = new List<FilmDB>();
 
             SqlConnection cnn = new SqlConnection("Data Source=10.0.5.102,1433;Initial Catalog=LAX_DB;User ID=sa; Password=Guest1234");
-            string sql = "SELECT * FROM Film WHERE FilmNomi = NOT NULL ";
-            SqlCommand cmd = new SqlCommand(sql, cnn);
-            SqlDataReader rdr;
-            string Output = "";
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Film", cnn);
+            DataSet ds = new DataSet();
+            cnn.Open();
+            da.Fill(ds);
 
-            //var model = new List<Film>();
-
-            using (cnn)
+            foreach(DataRow dr in ds.Tables[0].Rows)
             {
-                cnn.Open();
-                rdr = cmd.ExecuteReader();
-
-                while (rdr.Read())
-                {
-                    Output = Output + rdr.GetValue(0);
-
-                    //    var Film = new Film();
-                    //    Film.FilmTitel = rdr["FilmTitel"];
-                    //    Film.FilmInstruk = rdr["FilmInstruk"];
-                    //    Film.FilmUdgiv = rdr["FilmUdgiv"];
-                    //    Film.FilmOm = rdr["FilmOm"];
-                    //    Film.FilmNomi = rdr["FilmNomi"];
-                    //    Film.NomiVundet = rdr["NomiVundet"];
-
-                    //    model.Add(Film);
-                    //}
-                }
+                Film.Add(new FilmDB() { FilmID = Convert.ToInt32(dr[0]), FilmTitel = dr[1].ToString(), FilmInstruk = dr[2].ToString(), FilmUdgiv = Convert.ToInt32(dr[3]), FilmOm = dr[4].ToString(), FilmNomi = dr[5].ToString(), NomiVundet = Convert.ToInt32(dr[6]) });
             }
-
 
             return View();
         }
---
+++ LAX_AS_ASP/Models/FilmDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LAX_AS_ASP.Models
+{
+    public class FilmDB
+    {
+        public int FilmID { get; set; }
+        public string FilmTitel { get; set; }
+        public string FilmInstruk { get; set; }
+        public int FilmUdgiv { get; set; }
+        public string FilmOm { get; set; }
+        public string FilmNomi { get; set; }
+        public int NomiVundet { get; set; }
+    }
+}
